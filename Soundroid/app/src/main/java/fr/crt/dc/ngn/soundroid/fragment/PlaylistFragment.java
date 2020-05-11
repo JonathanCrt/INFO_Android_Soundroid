dda7696a044149da8820bcc142017092d037798f
MelissaDaCosta <mel-dacosta@hotmail.fr>
2020-05-11 19:14:24 +0200

    add alert dialog create playlist

--- Soundroid/app/src/main/java/fr/crt/dc/ngn/soundroid/fragment/PlaylistFragment.java
+++ Soundroid/app/src/main/java/fr/crt/dc/ngn/soundroid/fragment/PlaylistFragment.java
@@ -1,12 +1,22 @@
 package fr.crt.dc.ngn.soundroid.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 import fr.crt.dc.ngn.soundroid.R;
 
@@ -16,33 +26,14 @@ import fr.crt.dc.ngn.soundroid.R;
  * create an instance of this fragment.
  */
 public class PlaylistFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private Button addPplaylist;
     public PlaylistFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PlaylistFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static PlaylistFragment newInstance(String param1, String param2) {
         PlaylistFragment fragment = new PlaylistFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,16 +41,38 @@ public class PlaylistFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_playlist, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_playlist, container, false);
+        this.addPplaylist = fragmentView.findViewById(R.id.btn_add_playlist);
+        this.addPplaylist.setOnClickListener(v->{
+
+            Log.i("PLAYLIST FRAGMENT", "ADD PLAYLIS");
+            Toast.makeText(this.getContext(), "ADD PLAYLIST", Toast.LENGTH_SHORT).show();
+            AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(getActivity()));
+            builder.setMessage("Nom de la playlist");
+            final EditText tvPlaylistName = new EditText(getActivity());
+            builder.setView(tvPlaylistName);
+            builder.setNegativeButton("Cancel", null);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String playlistName = tvPlaylistName.getText().toString();
+                    Log.i("PLAYLIST FRAGMENT", "PLAYLIS NAME : " + playlistName);
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+            alertDialog.getButton(alertDialog.BUTTON_NEGATIVE).setTextColor(ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark));
+            alertDialog.getButton(alertDialog.BUTTON_POSITIVE).setTextColor(ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark));
+
+
+
+        });
+        return fragmentView;
     }
 }
